028e2212c9e62ee90bda47dd1597a9e271e71f80
DevRamona <r.ingabire@alustudent.com>
2024-08-22 09:46:48 +0200

    changes

--- calculateAverage.js
+++
@@ -1,8 +0,0 @@
-const calculateAverage = (numbers) => {
-    const sumOfNumbers = numbers.reduce(
-      (accumulator, value) => accumulator + value,
-      0
-    );
-    return sumOfNumbers / numbers.length;
-  };
-  
